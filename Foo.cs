fc2bbf467a6e0f3e03b710ac7fd846f9c1f75d70
BaturaDmitry <koonnot@mail.ru>
2021-10-13 10:58:09 +0300

    method with inner method of bar

---
+++ Foo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace mpp1
+{
+    public class Foo
+    {
+        private Bar _bar;
+        private ITracer _tracer;
+
+        public Foo(ITracer tracer)
+        {
+            _tracer = tracer;
+            _bar = new Bar(_tracer);
+        }
+
+        public void MyMethod()
+        {
+            _tracer.StartTrace();
+
+            _bar.InnerMethod();
+
+            _tracer.StopTrace();
+        }
+
+        public void NotMyMethod()
+        {
+            _tracer.StartTrace();
+
+            Thread.Sleep(50);
+            _bar.InnerMethod();
+
+            _tracer.StopTrace();
+        }
+    }
+}
