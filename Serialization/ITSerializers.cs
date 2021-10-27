c458d75bb35d640691b85cecf572918041eafdab
BaturaDmitry <koonnot@mail.ru>
2021-10-27 12:42:11 +0300

    add interface for serializers

---
+++ Serialization/ITSerializers.cs
@@ -0,0 +1,10 @@
+﻿using mpp1.TracerLib;
+
+namespace mpp1.Serialization
+{
+    public interface ITSerializers
+    {
+        public string ToXml(TraceResult result);
+        public string ToJson(TraceResult result);
+    }
+}
