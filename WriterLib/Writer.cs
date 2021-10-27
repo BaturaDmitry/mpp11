b8e9bb6b9aad15da7e04d334714255e1c47cb0b6
BaturaDmitry <koonnot@mail.ru>
2021-10-27 12:44:21 +0300

    add writer

---
+++ WriterLib/Writer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace mpp1.WriterLib
+{
+    public class Writer:ITWriter
+    {
+        private string Path = "C:\\Users\\37529\\RiderProjects\\mpp1\\mpp1\\XmlAndJson\\";
+        public void ToConsole(string text)
+        {
+            Console.OpenStandardOutput();
+            Console.WriteLine(text+"\n");
+        }
+
+        public void ToFile(string text, string fileName)
+        {
+            System.IO.File.WriteAllText(Path+fileName, text);
+            Console.WriteLine("Writing to the file was successful.\n");
+        }
+    }
+}
