7195bdab40270924206604274b81dbd7fbc2f18c
BaturaDmitry <koonnot@mail.ru>
2021-10-27 12:42:55 +0300

    add interface thread trace

---
+++ TracerLib/ITThreadTrace.cs
@@ -0,0 +1,10 @@
+﻿namespace mpp1.TracerLib
+{
+    public interface ITThreadTrace
+    {
+        public void StartTrace();
+        /*public void StopTrace();*/
+        public ThreadRes GetTraceRes();
+        
+    }
+}
